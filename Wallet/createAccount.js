d0e63938c45192febae5920f71353f198cf28cba
Shivva <matthieumariejoseph@gmail.com>
2020-04-24 11:35:46 +0200

    Create Wallet logic

---
+++ Wallet/createAccount.js
@@ -0,0 +1,17 @@
+import Web3 from 'web3';
+
+class CreateAccount {
+
+    password = 'Password';
+
+    constructor() {
+
+    }
+
+    store() {
+        
+    }
+}
+
+
+export default CreateAccount;
