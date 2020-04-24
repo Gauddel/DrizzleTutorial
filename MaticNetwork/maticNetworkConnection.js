e4668a7428e15bfa3c72a282003989953bc9e850
Shivva <matthieumariejoseph@gmail.com>
2020-04-24 10:09:06 +0200

    Segmentation in different part

---
+++ MaticNetwork/maticNetworkConnection.js
@@ -0,0 +1,19 @@
+import Matic from "maticjs";
+import Web3 from "web3";
+
+// Implemnent connection to 
+class MaticNetworkConnection {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (<div></div>);
+    }
+}
+
+export default MaticNetworkConnection;
