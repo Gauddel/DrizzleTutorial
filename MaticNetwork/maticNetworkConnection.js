20148951818c2edeeb528ad47423f0a64d7c7fa9
Shivva <matthieumariejoseph@gmail.com>
2020-04-24 11:33:43 +0200

    implementation of Matic wrapper

--- MaticNetwork/maticNetworkConnection.js
+++ MaticNetwork/maticNetworkConnection.js
@@ -3,16 +3,7 @@ import Web3 from "web3";
 
 // Implemnent connection to 
 class MaticNetworkConnection {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-
-        };
-    }
-
-    render() {
-        return (<div></div>);
+    constructor() {        
     }
 }
 
